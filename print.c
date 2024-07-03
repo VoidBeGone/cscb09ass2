37575ca6f20f96bf7e08db97aaea5d8d3dc3f21a
Peter Seunghoon Yoo <yoopete1@mathlab.utsc.utoronto.ca>
2024-07-03 19:07:29 -0400

    HELLO

--- print.c
+++ print.c
@@ -12,9 +12,13 @@ int main(int argc, char **argv)
     perror("fopen");
     return 1;
   }
-
   // read-print loop
-
+  //Okay so i am supposed to implement from here onwards
+  record new_record;
+  while (fread(&new_record, sizeof(record), 1, f) == 1){
+    int name_len = (int) new_record.name_len;
+    printf("%.*s has %d sunspots\n",name_len, new_record.name, new_record.sunspots );
+  }
   fclose(f);
   return 0;
 }
--- record.c
+++ record.c
@@ -3,10 +3,43 @@
 #include "record.h"
 
 void set_sunspots(FILE *f, const char *name, unsigned short sunspots)
-{
+{  if (f==NULL) return;
+   record *r;
+   int x = fseek(f, 0, SEEK_SET);
+   if (x != 0) return ;
+
+   while (fread(r, sizeof(record), 1, f) == 1){
+     int size = (int) r->name_len;
+     char names[size];
+     strcpy(names, r->name);
+     if (strcmp(names,name) == 0){
+       r->sunspots = sunspots;
+       return;
+     }
+   }
+   x = fseek(f,0,SEEK_END)
+   if (x != 0) return;
+   strcpy(r->name,name);
+   r->sunspots = sunspots;
+   r->name_len = strlen(name);
+   fread(r, sizeof(record), 1, f);
+   return;
 }
 
 int get_sunspots(FILE *f, const char *name, unsigned short *psunspots)
 {
-  return -1;
+  if (f == NULL) return 0;
+  record *r;
+  if (fseek(f, 0, SEEK_SET) != 0) return 0;
+
+  while (fread(r, sizeof(record), 1, f) == 1){
+    int size = (int) -> r->name_len;
+    char names[size];
+    strcpy(names, r->name);
+    if (strcmp(names, name) == 0 ){
+      *psunspots = r->sunspots;
+      return 1;
+    }
+  }
+  return 0;
 }
