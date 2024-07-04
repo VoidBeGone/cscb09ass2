5cb6f88572d8ae6e59f97e73cb3f7febc0e4f2e8
Peter Seunghoon Yoo <yoopete1@mathlab.utsc.utoronto.ca>
2024-07-03 20:02:06 -0400

    he

--- record.c
+++ record.c
@@ -2,42 +2,35 @@
 #include <string.h>
 #include "record.h"
 
-void set_sunspots(FILE *f, const char *name, unsigned short sunspots)
-{  if (f==NULL) return;
-   record *r;
-   int x = fseek(f, 0, SEEK_SET);
-   if (x != 0) return ;
-
-   while (fread(r, sizeof(record), 1, f) == 1){
-     int size = (int) r->name_len;
-     char names[size];
-     strcpy(names, r->name);
-     if (strcmp(names,name) == 0){
-       r->sunspots = sunspots;
-       return;
-     }
-   }
-   x = fseek(f,0,SEEK_END)
-   if (x != 0) return;
-   strcpy(r->name,name);
-   r->sunspots = sunspots;
-   r->name_len = strlen(name);
-   fread(r, sizeof(record), 1, f);
-   return;
+void set_sunspots(FILE *f, const char *name, unsigned short sunspots){
+  if (f==NULL || fseek(f,0,SEEK_SET) != 0) return;
+  record r;
+  while (fread(&r, sizeof(record), 1, f) == 1){
+    if (strlen(name) == r.name_len && strncmp(name, r.name, strlen(name)) == 0){
+      r.sunspots = sunspots;
+      fseek(f, -(sizeof(record)), SEEK_CUR);
+      fwrite(&r, sizeof(record), 1, f);
+      return;
+    }
+  }
+  if (fseek(f, 0, SEEK_END) != 0) return;
+  size_t size = strlen(name);
+  if (size > NAME_LEN_MAX) size = NAME_LEN_MAX;
+  memcpy(r.name, name, size); 
+  r.name_len = size;
+  r.sunspots = sunspots;
+  fwrite(&r, sizeof(record), 1, f);
+  return;
 }
 
 int get_sunspots(FILE *f, const char *name, unsigned short *psunspots)
 {
-  if (f == NULL) return 0;
-  record *r;
-  if (fseek(f, 0, SEEK_SET) != 0) return 0;
+  if (f == NULL || fseek(f, 0, SEEK_SET) != 0) return 0;
+  record r;
 
-  while (fread(r, sizeof(record), 1, f) == 1){
-    int size = (int) -> r->name_len;
-    char names[size];
-    strcpy(names, r->name);
-    if (strcmp(names, name) == 0 ){
-      *psunspots = r->sunspots;
+  while (fread(&r, sizeof(record), 1, f) == 1){
+    if (strlen(name) == r.name_len && strncmp(name, r.name, strlen(name)) == 0 ){
+      *psunspots = r.sunspots;
       return 1;
     }
   }
